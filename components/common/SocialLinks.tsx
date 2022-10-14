c099e9d86c677f0e16320a53ae549b8ed7a1e7f9
Ilies Paul <pauldaniel.ilies@gmail.com>
2022-10-14 10:15:35 +0300

    feat: replace image element

--- components/common/SocialLinks.tsx
+++ components/common/SocialLinks.tsx
@@ -1,19 +1,21 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import Link from "next/link";
+import Image from "next/image";
+
 const SocialLinks = () => {
   return (
     <Flex gap="1rem">
       <Box>
         <Link href="https://www.linkedin.com/in/iliespauldaniel" passHref>
           <a target="_blank" rel="noopener noreferrer">
-            <Image src="/linkedin.svg" alt="linkedin" w="35px" h="35px" />
+            <Image src="/linkedin.svg" alt="linkedin" width={35} height={35} />
           </a>
         </Link>
       </Box>
       <Box>
         <Link href="https://github.com/paul-ilies" passHref>
           <a target="_blank" rel="noopener noreferrer">
-            <Image src="/githubIcon.svg" alt="github" w="35px" h="35px" />
+            <Image src="/githubIcon.svg" alt="github" width={35} height={35} />
           </a>
         </Link>
       </Box>
